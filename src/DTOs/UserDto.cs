5e657f8dc70e58b810f5fec1e1d15da05a6864cc
FernandoChav <fpatric25@gmail.com>
2024-10-02 20:00:41 -0300

    feat se agregan el codigo de User en model y UserDto con sus correspondientes validaciones

--- src/DTOs/UserDto.cs
+++ src/DTOs/UserDto.cs
@@ -2,11 +2,21 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using System.ComponentModel.DataAnnotations;
 namespace Catedra1.src.DTOs
 {
     public class UserDto
     {
-        
+        [Required]
+        [StringLength (100, MinimumLength = 3, ErrorMessage = "Name must be between 3 and 100 characters")]
+        public string Name { get; set; } = string.Empty;
+        [Required]
+        [RegularExpression(@"^[^@\s]+@[^@\s]+\.[^@\s]+$", ErrorMessage = "El correo electrónico no es válido")]
+        public string Email { get; set; } = string.Empty;
+        [Required]
+        [RegularExpression("^(masculino|femenino|otro)$", ErrorMessage = "Los generos permitidos son: 'masculino', 'femenino' o 'otro'.")]
+        public string Gender { get; set; } = string.Empty;
+        [Required]
+        public DateOnly Birthdate { get; set; } = new DateOnly();
     }
 }
--- src/models/User.cs
+++ src/models/User.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,17 @@ namespace Catedra1.src.models
 {
     public class User
     {
-        
+        public int Id { get; set; }
+        [Required]
+        [StringLength (100, MinimumLength = 3, ErrorMessage = "Name must be between 3 and 100 characters")]
+        public string Name { get; set; } = string.Empty;
+        [Required]
+        [RegularExpression(@"^[^@\s]+@[^@\s]+\.[^@\s]+$", ErrorMessage = "El correo electrónico no es válido")]
+        public string Email { get; set; } = string.Empty;
+        [Required]
+        [RegularExpression("^(masculino|femenino|otro)$", ErrorMessage = "Los generos permitidos son: 'masculino', 'femenino' o 'otro'.")]
+        public string Gender { get; set; } = string.Empty;
+        [Required]
+        public DateOnly Birthdate { get; set; } = new DateOnly();
     }
 }
